8436ca60742cb3e332558f9d97ea0d948ebf18d9
Iulian Beleiu <i.beleiu@plan-net.com>
2020-06-14 16:42:15 +0300

    create account section

--- js/account.js
+++ js/account.js
@@ -1,79 +1,36 @@
-$(document).ready(function () {
-
+$.cookie('api_base_url', 'http://127.0.0.1:8000');
+$(document).ready(function () {        
     (function ($) {
         "use strict";
-
-
-        jQuery.validator.addMethod('answercheck', function (value, element) {
-            return this.optional(element) || /^\bcat\b$/.test(value)
-        }, "type the correct answer -_-");
-
-        // validate Create Account form
-        $(function () {
-            $('#createAccountForm').validate({
-                rules: {
-                    email: {
-                        required: true,
-                        email: true
-                    },
-                    name: {
-                        required: true,
-                    },
-                    password: {
-                        required: true,
-                        minlength : 5,
-                    },
-                    confirm_password: {
-                        required: true,
-                        minlength : 5,
-                        equalTo : "#password"
-                    }
-                },
-                messages: {
-                    email: {
-                        required: "Please add your email."
-                    },
-                    name: {
-                        required: "Please add your name.",
-                    },                    
-                    password: {
-                        required: "Please add your password.",
-                        minlength : "Password should be at least 5 characters.",
-                    },
-                    confirm_password: {
-                        required: "Please repeat your password.",
-                        minlength : "Password should be at least 5 characters.",
-                        equalTo : "Confirm password should match password."
-                    },
-                },
-                submitHandler: function (form) {
-                    $(form).ajaxSubmit({
-                        type: "POST",
-                        data: $(form).serialize(),
-                        dataType: 'json',
-                        url: "http://127.0.0.1:8000/api/user/create/",
-                        success: function (data) {
-                            $('#createAccountForm :input').attr('disabled', 'disabled');
-                            $('#createAccountForm').fadeTo("slow", 1, function () {
-                                $(this).find(':input').attr('disabled', 'disabled');
-                                $(this).find('label').css('cursor', 'default');
-                                $('#successCreateAccount').fadeIn()
-                                $('.modal').modal('hide');
-                                $('#successCreateAccount').modal('show');
-                            })
-                        },
-                        error: function (data) {
-                            $('#createAccountError').empty();
-                            $('#createAccountError').removeClass("d-none");
-
-                            $.each(data.responseJSON, function(key,valueObj){
-                                $('#createAccountError').append("<p>" + key + " : " + valueObj + "</p>");
-                            });
-                        }
-                    })
+        
+        //orders list
+        $.ajax({
+            url: "http://127.0.0.1:8000/api/order/cart/",
+            dataType: 'json',
+            type: "GET",
+            "headers": {
+              "Authorization": "Token " + $.cookie('token'),
+            },
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            error: function (err) {
+            },
+            success: function (data) {
+                var i = 0;
+                var cartTotal = 0
+                for (i=0;i<data.length;i++) {
+                    var html = '<tr>'
+                    html += '<td>'+data[i].items.length+'</td>'                    
+                    html += '<td>'+data[i].total+'</td>'
+                    html += '<td>'+data[i].created_at+'</td>'
+                    html += '<td><a href="http://127.0.0.1:8000/api/order/cart/'+data[i].id+'/receipt/">Download as PDF</a></td>'                    
+                    html += '</tr>'
+                    
+                    $('#checkoutTable tbody').prepend(html)
                 }
-            })
-        })
-
-    })(jQuery)
+            }
+        });
+    })(jQuery)    
 })
