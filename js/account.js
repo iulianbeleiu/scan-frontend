833613c7396a4f6426b4c51e35580d702e8e69e5
Iulian Beleiu <i.beleiu@plan-net.com>
2020-06-14 18:00:26 +0300

    download pdf for authenticated user, add field to adjust quantity in cart

--- js/account.js
+++ js/account.js
@@ -1,15 +1,15 @@
 $.cookie('api_base_url', 'http://127.0.0.1:8000');
-$(document).ready(function () {        
+$(document).ready(function () {
     (function ($) {
         "use strict";
-        
+
         //orders list
         $.ajax({
             url: "http://127.0.0.1:8000/api/order/cart/",
             dataType: 'json',
             type: "GET",
             "headers": {
-              "Authorization": "Token " + $.cookie('token'),
+                "Authorization": "Token " + $.cookie('token'),
             },
             xhrFields: {
                 withCredentials: true
@@ -19,18 +19,62 @@ $(document).ready(function () {
             },
             success: function (data) {
                 var i = 0;
-                var cartTotal = 0
-                for (i=0;i<data.length;i++) {
+                for (i = 0; i < data.length; i++) {
                     var html = '<tr>'
-                    html += '<td>'+data[i].items.length+'</td>'                    
-                    html += '<td>'+data[i].total+'</td>'
-                    html += '<td>'+data[i].created_at+'</td>'
-                    html += '<td><a href="http://127.0.0.1:8000/api/order/cart/'+data[i].id+'/receipt/">Download as PDF</a></td>'                    
+                    html += '<td>' + data[i].items.length + '</td>'
+                    html += '<td>$' + data[i].total + '</td>'
+                    html += '<td>' + data[i].created_at + '</td>'
+                    html += '<td><a class="pdf_download" id="'+data[i].id+'" href="#">Download as PDF</a></td>'
+                    html += '<td><a href="#" class="btn btn-primary height-auto btn-sm remove-item"  id="' + data[i].id + '">X</a></td>'
                     html += '</tr>'
-                    
+
                     $('#checkoutTable tbody').prepend(html)
                 }
             }
         });
-    })(jQuery)    
+    })(jQuery)
+
+    $(document).on('click', '.remove-item', function () {
+        alert('Are you sure you want to remove this item?');
+        $(this).closest('tr').remove();
+
+        $.ajax({
+            url: "http://127.0.0.1:8000/api/order/cart/" + this.id + '/',
+            type: "DELETE",
+            "headers": {
+                "Authorization": "Token " + $.cookie('token'),
+            },
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            error: function (err) {
+            }
+        });
+    });
+
+    $(document).on('click', '.pdf_download', function (e) {
+        e.preventDefault();
+
+        console.log(this.id)
+
+        $.ajax({
+            url: "http://127.0.0.1:8000/api/order/cart/"+this.id+"/receipt/",
+            type: "GET",
+            "headers": {
+                "Authorization": "Token " + $.cookie('token'),
+            },
+            xhrFields: {
+                withCredentials: true,
+                responseType: 'blob'
+            },
+            crossDomain: true,
+            success: function(blob){
+                var link=document.createElement('a');
+                link.href=window.URL.createObjectURL(blob);
+                link.download="Receipt-" + new Date() + ".pdf";
+                link.click();
+            }
+        });
+    });
 })
--- js/cart.js
+++ js/cart.js
@@ -1,27 +1,28 @@
 $.cookie('api_base_url', 'http://127.0.0.1:8000');
-$(document).ready(function () {        
+$(document).ready(function () {
     (function ($) {
         "use strict";
-                
+
+
         //add to cart
         $.ajax({
             url: "http://127.0.0.1:8000/api/order/cart-items/",
             dataType: 'json',
             type: "GET",
             "headers": {
-              "Authorization": "Token " + $.cookie('token'),
+                "Authorization": "Token " + $.cookie('token'),
             },
             xhrFields: {
                 withCredentials: true
             },
             crossDomain: true,
             error: function (err) {
-              
+
             },
             success: function (data) {
                 var i = 0;
                 var cartTotal = 0
-                for (i=0;i<data.results.length;i++) {
+                for (i = 0; i < data.results.length; i++) {
                     var total_price = 0
                     total_price = (parseInt(data.results[i].quantity) * parseFloat(data.results[i].price))
                     cartTotal += total_price
@@ -29,11 +30,13 @@ $(document).ready(function () {
                     html += '<td class="product-name">'
                     html += '<h2 class="h5 text-black">' + data.results[i].name + '</h2>'
                     html += '<td>' + data.results[i].price + '</td>'
-                    html += '<td>' + data.results[i].quantity + '</td>'
+                    html += '<td><div class="input-group mb-3" style="max-width: 120px;"><div class="input-group-prepend"><button class="btn btn-outline-primary js-btn-minus" type="button">&minus;</button></div>'
+                    html += '<input type="text" data-id="' + data.results[i].id + '" readonly class="form-control text-center" value="' + data.results[i].quantity + '" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">'
+                    html += '<div class="input-group-append"><button class="btn btn-outline-primary js-btn-plus" type="button">&plus;</button></div></div></td>'
                     html += '<td>$' + total_price + '</td>'
-                    html += '<td><a href="#" class="btn btn-primary height-auto btn-sm remove-item" data-quantity='+data.results[i].quantity+' data-total='+total_price+' id="'+data.results[i].id+'">X</a></td>'
+                    html += '<td><a href="#" class="btn btn-primary height-auto btn-sm remove-item" data-quantity=' + data.results[i].quantity + ' data-total=' + total_price + ' id="' + data.results[i].id + '">X</a></td>'
                     html += '</tr>'
-                    
+
                     $('#cartTable tbody').append(html)
                 }
                 $('#cart_total').val(cartTotal)
@@ -54,11 +57,11 @@ $(document).ready(function () {
         cart_total -= parseFloat($(this).attr("data-total"));
         $('#cart_total').val(cart_total)
         $('#cartTotal').html("$" + cart_total)
-        $.ajax({            
+        $.ajax({
             url: "http://127.0.0.1:8000/api/order/cart-items/" + this.id + '/',
             type: "DELETE",
             "headers": {
-              "Authorization": "Token " + $.cookie('token'),
+                "Authorization": "Token " + $.cookie('token'),
             },
             xhrFields: {
                 withCredentials: true
@@ -66,6 +69,65 @@ $(document).ready(function () {
             crossDomain: true,
             error: function (err) {
             }
-        });        
+        });
+    });
+
+    $(document).on('click', '.js-btn-minus', function (e) {
+        e.preventDefault();
+        var quantity = parseInt($(this).closest('.input-group').find('.form-control').val());
+        if (quantity > 1) {
+            if (quantity != 0) {
+                cart_item_id = $(this).closest('.input-group').find('.form-control').attr("data-id");
+                var body = {
+                    "quantity": quantity - 1
+                }
+                $.ajax({
+                    url: "http://127.0.0.1:8000/api/order/cart-items/" + cart_item_id + "/",
+                    dataType: 'json',
+                    data: body,
+                    type: "PATCH",
+                    "headers": {
+                        "Authorization": "Token " + $.cookie('token')
+                    },
+                    xhrFields: {
+                        withCredentials: true
+                    },
+                    crossDomain: true,
+                    success: function (data) {
+                        $('.number').html(parseInt($('.number').html(), 10) - 1)
+                        $('.number').html()
+                    }
+                });
+                $(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) - 1);
+            }
+        }
+    });
+
+    $(document).on('click', '.js-btn-plus', function (e) {
+        e.preventDefault();
+
+        var quantity = parseInt($(this).closest('.input-group').find('.form-control').val());
+        cart_item_id = $(this).closest('.input-group').find('.form-control').attr("data-id");
+        var body = {
+            "quantity": quantity + 1
+        }
+        $.ajax({
+            url: "http://127.0.0.1:8000/api/order/cart-items/" + cart_item_id + "/",
+            dataType: 'json',
+            data: body,
+            type: "PATCH",
+            "headers": {
+              "Authorization": "Token " + $.cookie('token')
+            },
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,        
+            success: function (data) {
+                $('.number').html(parseInt($('.number').html(), 10) + 1)
+                $('.number').html()
+            }
+          });    
+          $(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) + 1);     
     });
 })
